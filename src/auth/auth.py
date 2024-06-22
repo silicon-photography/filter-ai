ad5bbb651c21ee310ec85c8683a75fcd76b499b0
blake2703 <dickersonb27@yahoo.com>
2024-06-22 02:16:10 -0700

    Updating documentation

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -8,8 +8,13 @@ sys.path.append(os.getcwd())
 logger = setup_logger()
 
 
+def fetch_access_token() -> str:
+    """
+    Fetches a token for access to the Dropbox API
 
-def fetch_access_token():
+    Returns:
+        str: access token
+    """
     response = requests.post(url="https://api.dropbox.com/oauth2/token",
                              data={
                                  "grant_type": "refresh_token",
@@ -23,7 +28,13 @@ def fetch_access_token():
     else:
         raise Exception(f"Error obtaining access token: {response.json()}")
 
-def verify_access_token(access_token: str):
+def verify_access_token(access_token: str) -> None:
+    """
+    Checks to see if an access token is valid, if it is not it will create a new one
+
+    Args:
+        access_token (str): Dropbox API token
+    """
     try:
         dbx = dropbox.Dropbox(oauth2_access_token=access_token)
         logger.info(f"Verifying access token...")
