4a371cf2062f2ea1259a4fb933ded758019a8db3
Anisha Siva <anishasiva@Anishas-Laptop.local>
2024-07-17 20:19:22 -0700

    navbar done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
 
     return (
         //create a fixed header that stays at the top of the page
-      <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b
+      <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b
       border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
         <div className="flex items-center px-5 lg:px-7.5 xl:px-10
         max-lg:py-4">
