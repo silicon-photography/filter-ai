6fd15e8fa7cf7f33f26ff6e607ebff223c3c34da
Anisha Siva <anishasiva@Anishas-Laptop.local>
2024-07-18 11:03:21 -0700

    pic added hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -37,7 +37,24 @@ const Hero = () => {
                 Get Started
             </Button>
         </div>
-
+        <div className= "relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+            <div className="relative z-1 p-0.5 rounded-2xl
+            bg-conic-gradient">
+                <div className="relative bg-n-8 rounded-[1rem]">
+                    <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]"/>
+                    <div>
+                        <img
+                            src={robot}
+                            className="w-full"
+                            width={1440}
+                            height={1800}
+                            alt="hero"
+                        
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     </Section>
   );
