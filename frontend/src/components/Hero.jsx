9eef22a0862473b423de07c82385ed3835c46221
Anisha Siva <anishasiva@Anishas-Laptop.local>
2024-07-18 11:37:07 -0700

    repositioned main text

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { curve, heroBackground, robot } from "../assets";
 const Hero = () => {
   return (
   <Section
-  className="pt-[3rem] -mt-[5.25]"
+  className="pt-[12rem] -mt-[5.25rem]"
   crosses
   crossesOffset="lg:translate-y-[5.25rem]"
   customPaddings
@@ -42,17 +42,29 @@ const Hero = () => {
             bg-conic-gradient">
                 <div className="relative bg-n-8 rounded-[1rem]">
                     <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]"/>
-                    <div>
+                    <div className="aspect-[33-40] rounded-b-[0.9rem]
+                    overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
                         <img
-                            src={robot}
-                            className="w-full"
-                            width={1440}
-                            height={1800}
-                            alt="hero"
-                        
+                            src={robot} //here is what u can tweak to put ur own image
+                            className="w-full scale-[1.7]
+                            translate-y-[8%] md:scale-[1]
+                            md:-translate-y-[10%] lg:-translate-y-[23%]"
+                            width={1024}
+                            height={490}
+                            alt="AI"
                         />
                     </div>
                 </div>
+            </div>
+            <div className="absolute -top-[54%] left-1/2 w-[234%] 
+            -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+                <img 
+                src={heroBackground} //here is where u can change the background
+                className="w-full"
+                width={1440}
+                height={1800}
+                alt="hero"
+                />
             </div>
         </div>
     </div>
